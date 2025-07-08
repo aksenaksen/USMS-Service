17de166f423f3eaaafc4b5f4b2d6577a6a977b82
aksenaksen <ans109905@naver.com>
2025-07-08 18:18:19 +0900

    멀티모듈 구성 템플릿

---
+++ common/snowflake/src/test/java/io/aksenaksen/demo/multimodule/MultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aksenaksen.demo.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
